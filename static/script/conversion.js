429c55ded211d1b3d88c29d319e00dbf378a5817
Daniel <wdaniel.jimenez@gmail.com>
2020-11-07 18:50:43 -0600

    Creación del index, estilos y js

---
+++ static/script/conversion.js
@@ -0,0 +1,15 @@
+function convertir(){
+    let req = new XMLHttpRequest();
+    binario = document.getElementById("binario").value
+    document.getElementById("decimal").value = binario*2
+    document.getElementById("binario").value = ""
+    ruta = 'https://networkcalc.com/api/binary/'+binario
+    req.open('GET', ruta ,true);
+    req.onreadystatechange = function() {
+        if (req.readyState === XMLHttpRequest.DONE && req.status == 200) {
+            var respuesta = JSON.parse(req.responseText)
+            document.getElementById("decimal").value = respuesta.converted
+        }
+    };
+    req.send();
+}
